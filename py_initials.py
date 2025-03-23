79d344d0481c05946f275087120d85448587a346
jsonShadower999 <142488144+jsonShadower999@users.noreply.github.com>
2025-03-23 13:25:11 +0530

    Create py_initials.py

---
+++ py_initials.py
@@ -0,0 +1,84 @@
+INTERVIEW QUESTIONS ::::
+
+1... range vs xrange
+2... set vs frozenset
+3... bytes vs bytearray
+4... memoryview ? how to use it 
+5.. membership operator
+6.. remove vs pop
+7.. pop() vs pop(0)
+8.. remove vs discard vs pop
+9.. iter vs next
+10.. package vs modules vs classes
+11.. input() vs raw_input()
+12.. sort vs sorted 
+13.. magic methods
+14.. decorators
+15.. pop vs popitem vs del
+16.. Defaultdict vs ordereddict
+17.. map , filter , reduce
+18.. Generators vs return_statement
+19.. closure 
+20.. @staticmethod, @classmethod , @abstractmethod , @retry , @cache , @get_time , @atexit , @total_ordering
+21.. dunder method
+22 .. instance method , class method , static method 
+23.. optimise code 
+(use @profile , list cmp ) 
+24..  exec() , permutations(),partials() , choice() , askopenfilename() , enumerate() 
+25.. None vs NoneType 
+26.. chainMap
+27.. Logging concept 
+28.. copy shallow + deep 
+29.. design pattern 
+(factory pattern , proxy pattern +singelton pattern + composite pattern)
+30.. error vs warning vs exception 
+31... context manager  for multithreads 
+33.. memory management  in py ? GIL ?garbage_collector n ref_count ?BestFit Algorithm ?
+34., __init__ , __str__ , __del__ , __repr__
+FUNCTIONS USED IN STRING MANIPULATIONS ===>
+
+
+str.upper()
+str.lower()
+str.strip()
+str.replace(new_data,old_data)
+str.captialize()
+str.endsWith()
+str.startWith()
+str.find()
+str.swapcase()
+str.sort()
+str.sort(key=str.lower)
+str.sort(key=len)
+str.sort(key=lambda a:a[-1])
+
+
+
+List_cmp ways:::
+
+sq_no=[i**2 for i in range(5)]
+sq_dict=[i:i**2 for i in range(5)]
+
+Zip Function:
+list_numbers=[1,2,3,4,5]
+list_strs=["hello","github","with" ,"python", "zonals"]
+for i,j in zip(list_numbers,list_strs):
+          print(i,j)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
